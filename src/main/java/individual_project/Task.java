3c06664b1f6423a157665c5133e04b5eac3ca306
shach934 <tjukid@gmail.com>
2020-10-15 22:50:18 +0200

    class Task initial version
    Task defines by title, status, due date, project, description.
    Task should have a unique title.
    Status are defined by enum, doing, pending, ASAP, done.
    due date, is using Java Date,
    Task class is not taking care of the data format, it should be already properly checked before it is assigned to a Task.

---
+++ src/main/java/individual_project/Task.java
@@ -0,0 +1,45 @@
+package individual_project;
+
+import java.util.Date;
+
+enum Status{
+    DOING, PENDING, ASAP, DONE;
+}
+
+
+public class Task{
+    /**
+     * Task class:
+     * Construct a task with @param	 Title, @param Status, @param dueData, @param project, @param description.
+     * Function list: set@params,  show(), edit@param,
+     */
+    private String title;
+    private Status status;
+    private Date dueDate;
+    private String project;
+    private String description;
+
+    public void setTitle(String title){ this.title = title; }
+    public void setStatus(Status status){  this.status = status; }
+    public void setDueDate(Date date){ this.dueDate = date; }
+    public void setProject(String project) {  this.project = project; }
+    public void setDescription(String description) {  this.description = description; }
+
+    public String getTitle() { return title;}
+    public Status getStatus(){  return status;  }
+    public String getDueDate() { return dueDate.toString();}
+    public String getProject() { return project;}
+    public String getDescription() { return description;}
+
+    @Override
+    public String toString(){
+        StringBuilder taskInfo = new StringBuilder("Task: " + title + "\n");
+        taskInfo.append("Project: " + project + "\n");
+        taskInfo.append("Status: " + status + "\n");
+        taskInfo.append("Due Date: " + dueDate + "\n");
+        return taskInfo.toString();
+    }
+
+    public void markDone(){  this.status = Status.DONE;  }
+    public boolean equals(Task t2){  return title == t2.title; }
+}
